0df224e185f8121dd80ec3642f3eed479a687630
Kelsey Iafrate <kelseylafrate@Kelseys-MacBook-Pro.local>
2016-05-27 13:37:14 -0700

    XML
    Finished XML tutorial

--- Coursera/RPlayGround.R
+++ Coursera/RPlayGround.R
@@ -18,4 +18,51 @@ dateDownloaded
 
 #Import from XML
 
+#tags = labels: <start> ends with </start> or <empty tag /> 
+#elements are specific examples of tags
+#attributes = components of the label:
+#<img src= "jeff.jpg" alt="instructor"/>
+# See wikipedia page on XML
 
+library(XML)
+fileURL = "http://www.w3schools.com/xml/simple.xml"
+doc <- xmlTreeParse(fileURL, useInternal = TRUE)
+#wrapper element for the entire document
+rootNode <- xmlRoot(doc)
+#get name out
+xmlName(rootNode)
+#shows elements within rootNode
+names(rootNode)
+#directly access parts of document. 
+#Similar to accessing list elements
+
+#first subcomponent of the root node
+rootNode[[1]]
+#first subcomponent of the first subcomponent of the root node
+rootNode[[1]][[1]]
+
+#Programmatically extract parts of file
+
+#gets every single value of every single tagged element
+xmlSApply(rootNode,xmlValue)
+
+#get specific component using XPath language. Ugh: 
+#see www.stat.berkely.edu/~statcur/Workshop2/Presentations/XML.pdf
+
+#/node is top level node
+#//node is any level
+
+#gets items on the menu. Gets nodes with tags named name
+xpathSApply(rootNode,"//name",xmlValue)
+
+#gets prices
+xpathSApply(rootNode,"//price",xmlValue)
+
+#right click and selecet "view source" to see xml code on sites
+
+fileURL = "http://espn.go.com/nfl/team/_/name/bal/baltimore-ravens"
+#use htmlTreeParse when getting html file
+doc <- htmlTreeParse(fileURL,useInternal = TRUE)
+#li = list items, 
+scores <- xpathSApply(doc,"//li[@class='score']",xmlValue)
+teams <- xpathSApply(doc,"//li[@class='team-name']",xmlValue)
