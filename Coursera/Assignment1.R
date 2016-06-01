7d5302f1ddd06f1640e48be7a3e1f0edf29a6060
Kelsey Iafrate <kelseylafrate@Kelseys-MacBook-Pro.local>
2016-06-01 07:08:21 -0700

    Assignment 1
    Completed assignment 1.

--- Coursera/Assignment1.R
+++ Coursera/Assignment1.R
@@ -31,4 +31,28 @@ sum(mydf$Zip*mydf$Ext,na.rm=T)
 library(XML)
 url = "https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2Frestaurants.xml"
 download.file(url, destfile = "./assignments/Restaurants.xml",method = "curl")
-rests <- xmlTreeParse("./assignments/Restaurants.xml", useInternalNodes = T)
+rests <- xmlTreeParse("./assignments/Restaurants.xml", useInternalNodes = TRUE)
+rootNode <- xmlRoot(rests)
+xmlName(rootNode[[1]])
+names(rootNode[[1]])
+?xmlSApply
+rootNode[[1]][['row']]
+zipcodes<-xpathSApply(rootNode,"//zipcode" ,xmlValue)
+zips <- as.data.frame(zipcodes)
+zips %>% filter(zipcodes == '21231') %>% summarise(n())
+
+
+#5
+library(data.table)
+url = "https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2Fss06pid.csv"
+DT <- fread(url)
+class(DT)
+system.time(DT[,mean(pwgtp15),by=SEX])
+rowMeans(DT)[DT$SEX==1]; rowMeans(DT)[DT$SEX==2]
+system.time(tapply(DT$pwgtp15,DT$SEX,mean))
+system.time(sapply(split(DT$pwgtp15,DT$SEX),mean))
+system.time(mean(DT[DT$SEX==1,]$pwgtp15))
+            mean(DT[DT$SEX==2,]$pwgtp15)
+            
+#this is the fastest
+system.time(mean(DT$pwgtp15,by=DT$SEX))
