b800fdf8e50ad065ec18bd370c8e11f7f226bc3e
Kelsey Iafrate <kelseylafrate@Kelseys-MacBook-Pro.local>
2016-06-08 00:25:13 -0700

    Week 4 completion
    Finished the course, except I have to go back to assignment 2. This
    last week was much easier than the previous ones. I’m thinking about
    doing the course again. Hopefully a family member will pay for the
    certificate as a graduation present.

--- Coursera/Week4PlayGround.R
+++ Coursera/Week4PlayGround.R
@@ -92,13 +92,96 @@ str_trim("Jeff         ")
 #we need a way to express whitespace word boundaries, sets of literals,
 #the beginning and end of a line, and alternatives ("war" or "peace")
 #use metta characters
+
 #^i think matteches the start
+
 #$ mathes the end of the line
+
 #use [] to give a list of characters we will accept
 #then you can combine them
 #you can also match a range 
 
 #number followed by any letter
 #[0-9] [a-zA-Z]
+
 #the carrot ^ can also indicate what we don't want
 #[^?.]$ mathes any line that does not end in ? or .
+
+
+#regular expressions 2
+#mettacharacters
+#. is for any character
+#| for alternatives the or
+#can be combined
+#^[Gg]ood|[Bb]ad is starts with good or has bad in it
+#adding () will include everything
+#^([Gg]ood|[Bb]ad) will get things that start with good or bad
+
+#a ? after () inedicates it is opptional
+#[Gg]eorge( [Ww]\.)? [Bb]ush makes the w opptional
+#\ escaped the dot
+#* means any number including none
+#(.*) mathes anything in ()
+#+ means at least one
+#[0-9]+ (.*) [0-9]+
+#{} interval quantifiers
+#[Bb]ush( +[^ ]+ +){1,5} debate
+#4:41 look again
+#m.n means at least m but not more than n
+#m means exactly m
+#m, means at least m
+
+#() can also be used to remember matched text
+# +([a-zA-Z])+ +\1 +
+#at least one space, followed by at least one letter, followed
+#by at least on space, followed by the exact same string of letters,
+#followed by at least one space
+
+#meta characters are greedy, so it will take the longest possible string that
+#satisfies an expression
+#To make it less gredy
+#use a questionmark
+#^s(.*?)s
+#Good to use when you want to extract data from "unfriendly" sources
+
+
+
+#Working with dates
+d1 = date()
+d1
+
+class(d1)
+
+d2 = Sys.Date()
+d2
+class(d2)
+
+#formatting
+format(d2,"%a %b %d")
+#%A unabbreviated weekday
+#%m is mont %b is abbreviated month
+#%y is 2 digit year %Y is 4 digit year
+??dmy
+library(lubridate)
+x = c("1jan1960","2jan1960","31mar1960","30jul1960")
+z = dmy(x)
+z
+z[1]-z[2]
+as.numeric(z[1]-z[2])
+weekdays(d2)
+months(d2)
+julian(d2)
+?Sys.timezone
+wday(dmy(x)[1])
+wday(dmy(x)[1],label = TRUE)
+#want in POSIXlt
+
+
+
+#Data resources
+#data.gov/opendatasites
+#gapminder.org
+#surveys
+#asdfree.com
+#infochimps.com/marketplace
+#blog.mortardata.com/post/67652898761/6-dataset-list-curated-by-data-scientists
